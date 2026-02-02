e13a8519e67bdd8a62248582e6f13eaf3f01b398
NurgazievichR <zalalidinovroma@gmail.com>
2026-02-02 16:19:17 +0600

    static files added

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Где находятся исходные статические файлы (для разработки)
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Куда collectstatic собирает все файлы (для production)
 
 # Media files
 MEDIA_URL = '/media/'
