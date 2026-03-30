a090a2d9a4610cf39dd13e970e0a604346befb02
“Burkhan <kb12362@auca.kg>
2026-03-31 00:37:08 +0600

    Submit translation for footer, services. Add privacy policy, terms

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,6 +6,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
     path('contacts/', views.contacts, name='contacts'),
     path('chatbot-lead/', views.chatbot_lead, name='chatbot_lead'),
 ]
